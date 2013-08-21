33bfd4db213ff7418ebb0903f7a9293760cfa819
Rob Clark <robclark@ubuntu.(none)>
2013-08-20 18:18:43 -0700

    static pages setup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = "Ruby on Rails Map App"
+    if page _title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
