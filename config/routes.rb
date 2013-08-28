1795d75766be09c09dd6f1920b29379a9cea13cd
Rob Clark <robclark@ubuntu.(none)>
2013-08-27 23:13:52 -0700

    finished user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 MapApp::Application.routes.draw do
 
+  resources :users
   match '/signup',	to: 'users#new',		via: 'get'
   match '/help',	to: 'static_pages#help',	via: 'get'
   match '/about',	to: 'static_pages#about',	via: 'get'
